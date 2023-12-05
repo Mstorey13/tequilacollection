2b56a47b31002bd4db6e5a1edb339733fc1f3a51
Maggie Storey <maggiestorey@Maggies-Air.lan>
2023-12-05 15:48:22 -0800

    first commit

--- app/__init__.py
+++ app/__init__.py
@@ -19,7 +19,7 @@ app.register_blueprint(api)
 
 app.json_encoder = JSONEncoder
 app.config.from_object(Config)
-root_db.init(app)
+root_db.init_app(app)
 login_manager.init_app(app)
 ma.init_app(app)
 migrate = Migrate(app, root_db)
